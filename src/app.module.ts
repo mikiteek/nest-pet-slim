315da6be1fc28bdc786c0d1f279cf5c18ead0b53
mikiteek <mikiteek@gmail.com>
2021-02-18 21:51:58 +0200

    Init categories controller, set createCategory method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { DatabaseModule } from "./utils/database";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
