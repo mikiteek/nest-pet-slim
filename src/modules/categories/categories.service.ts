99846725dcb05f20e062e88b0bf9ceb2220f884f
mikiteek <mikiteek@gmail.com>
2021-02-19 13:00:28 +0200

    Implement create category, validation with validation class

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@nestjs/common";
+import {ConflictException, HttpStatus, Injectable} from "@nestjs/common";
 import {InjectModel} from '@nestjs/sequelize';
 import {Category} from "./category.model";
 import {CreateCategoryDto} from "./dbo/create-category.dto";
@@ -9,7 +9,12 @@ export class CategoriesService {
     @InjectModel(Category)
     private categoryModel: typeof Category,
   ) {}
+
   async create(categoryData: CreateCategoryDto): Promise<Category> {
+    const categoryExisted = await this.categoryModel.findOne({where: {name: categoryData.name}});
+    if (categoryExisted) {
+      throw new ConflictException({message: "Name must be unique"});
+    }
     const category = new Category();
     category.name = categoryData.name;
     return await category.save();
