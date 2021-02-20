70583ff38a7fa6894d62825215d9714cd184f809
mikiteek <mikiteek@gmail.com>
2021-02-20 14:56:04 +0200

    Refactor categories.service's method create, right tests for add category

--- src/modules/categories/category.model.ts
+++ src/modules/categories/category.model.ts
@@ -1,9 +1,10 @@
 import { Column, Model, Table, DataType } from "sequelize-typescript";
+import {CreateCategoryDto} from "./dbo/create-category.dto";
 
 @Table({
   timestamps: false,
 })
-export class Category extends Model <Category> {
+export class Category extends Model <CreateCategoryDto> {
   @Column({
     type: DataType.STRING,
     unique: true,
